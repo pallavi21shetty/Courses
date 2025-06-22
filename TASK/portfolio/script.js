aab3b4a71812bf2a3d71bd4aff53ec082ff91b15
pallavi21shetty <pallavishetty473@gmail.com>
2025-06-22 22:12:19 +0530

    modified portfolio

--- TASK/portfolio/script.js
+++ TASK/portfolio/script.js
@@ -1,2 +1,34 @@
-// Optional: Handle contact form or other custom JS
-console.log("Portfolio loaded");
+
+document.getElementById('contactBtn').addEventListener('click', function () {
+  alert('Thank you! Your message has been submitted.');
+});
+
+const homeButton = document.querySelector('.button');
+homeButton.addEventListener('mouseenter', () => {
+  homeButton.style.backgroundColor = '#0d6efd';
+});
+homeButton.addEventListener('mouseleave', () => {
+  homeButton.style.backgroundColor = ''; 
+});
+
+
+
+const messageBox = document.getElementById('messageBox');
+const charCount = document.getElementById('charCount');
+messageBox.addEventListener('input', () => {
+  charCount.textContent = `${messageBox.value.length} characters`;
+});
+
+
+
+  const text = "Web Developer";
+  let i = 0;
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("typedText").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+  window.onload = typeWriter;
+
